70a6e46f638c07e41ab9b4588e1ddc46f275e37c
Denis <denis.were@student.moringaschool.com>
2023-06-13 09:09:26 +0300

    home page styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -11,30 +12,41 @@ const Home = () => {
       <div className="about">
         <h2 className="home-h2">About Us</h2>
         <p className="home-p">
-          Flex Fitness is dedicated to helping you achieve your fitness goals. Our app provides a comprehensive solution for tracking your workouts and managing your exercise routine.
+          Flex Fitness is dedicated to helping you achieve your fitness goals. Our app provides a comprehensive solution for tracking your workouts and managing your exercise routine.{' '}
+          <Link to="/login" className="home-a">
+            Log in
+          </Link>{' '}
+          to get started.
         </p>
       </div>
 
       <div className="features">
-        <h2 className="home-h2">Key Features</h2>
+        <h2 className="home-h3">Key Features</h2>
         <ul className="home-ul">
-          <li className="home-li">Create Custom Workouts: Build personalized workout routines tailored to your specific needs and preferences.</li>
-          <li className="home-li">Browse through Exercises: Explore a wide range of exercises and find detailed information on how to perform them correctly.</li>
-          {/* Add more key features here */}
+          <li className="home-li">
+            Create Custom <Link to="/workouts">Workouts</Link>: Build personalized workout routines tailored to your specific needs and preferences.
+          </li>
+          <li className="home-li">
+            Browse through <Link to="/exercises">Exercises</Link>: Explore a wide range of exercises and find detailed information on how to perform them correctly.
+          </li>
         </ul>
       </div>
 
-      <div className="contact">
+      <footer className="footer">
         <h2 className="home-h2">Contact Us</h2>
-        <p className="home-p">
-          Connect with us on social media for any questions or feedback:
-        </p>
+        <p className="home-p">Connect with us on social media for any questions or feedback:</p>
         <ul className="home-ul">
-          <li className="home-li">Facebook: <a className="home-a" href="https://www.facebook.com/flexfitness">Flex Fitness</a></li>
-          <li className="home-li">Twitter: <a className="home-a" href="https://www.twitter.com/flexfitness">Flex Fitness</a></li>
-          <li className="home-li">Instagram: <a className="home-a" href="https://www.instagram.com/flexfitness">Flex Fitness</a></li>
+          <li className="home-li">
+            Facebook: <a className="home-a" href="https://www.facebook.com/flexfitness">Flex Fitness</a>
+          </li>
+          <li className="home-li">
+            Twitter: <a className="home-a" href="https://www.twitter.com/flexfitness">Flex Fitness</a>
+          </li>
+          <li className="home-li">
+            Instagram: <a className="home-a" href="https://www.instagram.com/flexfitness">Flex Fitness</a>
+          </li>
         </ul>
-      </div>
+      </footer>
     </div>
   );
 };
