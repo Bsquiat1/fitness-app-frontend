d4df7ae607c03dbe22c4a7c7947222662828d48a
Denis <denis.were@student.moringaschool.com>
2023-06-13 08:05:01 +0300

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
 
-
 const Home = () => {
   return (
-    <div style={containerStyle}>
-     
-      
-      <div style={contentStyle}>
-        <h1>Welcome to Flex Fitness</h1>
-        <p>Get fit and stay healthy with our workout and exercise tracking app.</p>
+    <div className="wrapper">
+      <div className="heading">
+        <h1 className="home-h1">Welcome to Flex Fitness</h1>
+        <p className="home-p">Get fit and stay healthy with our workout and exercise tracking app.</p>
+      </div>
+
+      <div className="about">
+        <h2 className="home-h2">About Us</h2>
+        <p className="home-p">
+          Flex Fitness is dedicated to helping you achieve your fitness goals. Our app provides a comprehensive solution for tracking your workouts and managing your exercise routine.
+        </p>
+      </div>
+
+      <div className="features">
+        <h2 className="home-h2">Key Features</h2>
+        <ul className="home-ul">
+          <li className="home-li">Create Custom Workouts: Build personalized workout routines tailored to your specific needs and preferences.</li>
+          <li className="home-li">Browse through Exercises: Explore a wide range of exercises and find detailed information on how to perform them correctly.</li>
+          {/* Add more key features here */}
+        </ul>
+      </div>
+
+      <div className="contact">
+        <h2 className="home-h2">Contact Us</h2>
+        <p className="home-p">
+          Connect with us on social media for any questions or feedback:
+        </p>
+        <ul className="home-ul">
+          <li className="home-li">Facebook: <a className="home-a" href="https://www.facebook.com/flexfitness">Flex Fitness</a></li>
+          <li className="home-li">Twitter: <a className="home-a" href="https://www.twitter.com/flexfitness">Flex Fitness</a></li>
+          <li className="home-li">Instagram: <a className="home-a" href="https://www.instagram.com/flexfitness">Flex Fitness</a></li>
+        </ul>
       </div>
     </div>
   );
-};
-
-
-const containerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-};
-
-const headerStyle = {
-  background: '#f8f8f8',
-  padding: '20px',
-  textAlign: 'center',
-};
-
-const headerTextStyle = {
-  fontSize: '48px',
-  fontFamily: 'Old English Text MT, serif',
-};
-
-const contentStyle = {
-  textAlign: 'center',
 };
 
 export default Home;
--- src/components/WorkoutPlanner.jsx
+++ src/components/WorkoutPlanner.jsx
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from 'react';
 const WorkoutPlanner = () => {
   const [exercises, setExercises] = useState([]);
   const [selectedExercise, setSelectedExercise] = useState('');
+  const [sets, setSets] = useState(0);
+  const [reps, setReps] = useState(0);
+  const [duration, setDuration] = useState(0);
   const [workoutExercises, setWorkoutExercises] = useState([]);
 
   useEffect(() => {
@@ -17,10 +20,32 @@ const WorkoutPlanner = () => {
     setSelectedExercise(e.target.value);
   };
 
+  const handleSetsChange = (e) => {
+    setSets(parseInt(e.target.value, 10));
+  };
+
+  const handleRepsChange = (e) => {
+    setReps(parseInt(e.target.value, 10));
+  };
+
+  const handleDurationChange = (e) => {
+    setDuration(parseInt(e.target.value, 10));
+  };
+
   const handleAddExercise = () => {
     const exerciseToAdd = exercises.find((exercise) => exercise.id === selectedExercise);
     if (exerciseToAdd) {
-      setWorkoutExercises([...workoutExercises, exerciseToAdd]);
+      const exerciseWithDetails = {
+        ...exerciseToAdd,
+        sets,
+        reps,
+        duration,
+      };
+      setWorkoutExercises([...workoutExercises, exerciseWithDetails]);
+      setSelectedExercise('');
+      setSets(0);
+      setReps(0);
+      setDuration(0);
     }
   };
 
@@ -37,12 +62,20 @@ const WorkoutPlanner = () => {
             </option>
           ))}
         </select>
+        <label>Sets:</label>
+        <input type="number" value={sets} onChange={handleSetsChange} />
+        <label>Reps:</label>
+        <input type="number" value={reps} onChange={handleRepsChange} />
+        <label>Duration (in seconds):</label>
+        <input type="number" value={duration} onChange={handleDurationChange} />
         <button onClick={handleAddExercise}>Add Exercise</button>
       </div>
       <h3>Workout Exercises:</h3>
       <ul>
         {workoutExercises.map((exercise) => (
-          <li key={exercise.id}>{exercise.name}</li>
+          <li key={exercise.id}>
+            {exercise.name} - Sets: {exercise.sets}, Reps: {exercise.reps}, Duration: {exercise.duration} seconds
+          </li>
         ))}
       </ul>
     </div>
