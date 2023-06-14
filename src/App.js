cc4ff09f273f4bde33b7cede958c17e963b04ee2
Denis <denis.were@student.moringaschool.com>
2023-06-14 21:50:24 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
@@ -7,12 +7,11 @@ import WorkoutPlanner from './components/WorkoutPlanner';
 import Exercises from './components/Exercises';
 import Login from './components/Login';
 import Register from './components/Register';
-import SearchBar from './components/Searchbar';
 import Navbar from './components/Navbar';
 // import MagicMouseComponent from './components/MagicMouseComponent';
 
 const App = () => {
-  const [filteredExercises, setFilteredExercises] = useState([]);
+  
 
   
   return (
@@ -25,7 +24,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/workouts" element={<WorkoutPlanner />} />
-          <Route path="/exercises" element={<Exercises filteredExercises={filteredExercises} />} />
+          <Route path="/exercises" element={<Exercises  />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
