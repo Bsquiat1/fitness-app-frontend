c1a33cdbebeb55ff7fb5057e43f8bf12920608e6
Denis <denis.were@student.moringaschool.com>
2023-07-08 12:30:54 +0300

    modal

--- src/components/WorkoutPlanner.jsx
+++ src/components/WorkoutPlanner.jsx
@@ -7,6 +7,7 @@ const WorkoutPlanner = () => {
   const [reps, setReps] = useState(0);
   const [duration, setDuration] = useState(0);
   const [workouts, setWorkouts] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     fetch('/workouts')
@@ -24,35 +25,40 @@ const WorkoutPlanner = () => {
 
   const handleExerciseChange = (e) => {
     setSelectedExercise(e.target.value);
-    
   };
 
   const handleSetsChange = (e) => {
     setSets(parseInt(e.target.value, 10));
-    
   };
 
   const handleRepsChange = (e) => {
     setReps(parseInt(e.target.value, 10));
-    
   };
 
   const handleDurationChange = (e) => {
     setDuration(parseInt(e.target.value, 10));
-    
   };
-  
 
   const handleAddExercise = () => {
-    
-  
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    setSelectedExercise('');
+    setSets(0);
+    setReps(0);
+    setDuration(0);
+  };
+
+  const handleModalSubmit = () => {
     fetch('/workouts', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        name: selectedExercise,
+        workout_name: selectedExercise,
         sets,
         reps,
         duration,
@@ -67,41 +73,36 @@ const WorkoutPlanner = () => {
         }
       })
       .then((data) => {
-        // Update the workouts state with the added exercise
         setWorkouts([...workouts, data]);
-  
-        // Reset input values
-        setSelectedExercise('')
+        setShowModal(false);
+        setSelectedExercise('');
         setSets(0);
         setReps(0);
         setDuration(0);
       })
       .catch((error) => console.log(error));
   };
-  
-  
-  const handleDeleteWorkout = (exerciseId) => {
-    
 
+  const handleDeleteWorkout = (exerciseId) => {
     fetch(`/workouts/${exerciseId}`, {
       method: 'DELETE',
     })
       .then((response) => response.json())
       .then((data) => {
         console.log('Workout deleted successfully!');
+        setWorkouts(workouts.filter((workout) => workout.id !== exerciseId));
       })
       .catch((error) => console.log(error));
   };
 
   const handleDeleteAllWorkouts = () => {
-    setWorkouts([]);
-
     fetch('/workouts', {
       method: 'DELETE',
     })
       .then((response) => response.json())
       .then((data) => {
         console.log('All workouts deleted successfully!');
+        setWorkouts([]);
       })
       .catch((error) => console.log(error));
   };
@@ -110,35 +111,52 @@ const WorkoutPlanner = () => {
     <div className="workout-planner">
       <h2>Workout Planner</h2>
       <div className="input-group">
-        <label>Exercise name:</label>
-        <input
-          type="text"
-          value={selectedExercise}
-          onChange={handleExerciseChange}
-          placeholder="Enter exercise name"
-        />
+        
+        
 
-        <label>Sets:</label>
-        <input type="number" value={sets} onChange={handleSetsChange} />
-        <label>Reps:</label>
-        <input type="number" value={reps} onChange={handleRepsChange} />
-        <label>Duration (in minutes):</label>
-        <input type="number" value={duration} onChange={handleDurationChange} />
-        <button className='btn' onClick={handleAddExercise}>Add Exercise</button>
+        <div className='button-container'>
+          <button className="btn" onClick={handleAddExercise}>
+          Add Exercise
+        </button>
+        </div>
+        
       </div>
       <h3>Workout Exercises:</h3>
       <ul className="workout-list">
         {workouts.map((workout) => (
           <li key={workout.id}>
-            {workout.name} - Sets: {workout.sets}, Reps: {workout.reps}, Duration: {workout.duration} minutes
-            <button className='delete-button' onClick={() => handleDeleteWorkout(workout.id)}><span class="X"></span>
-  <span class="Y"></span>
-  <div class="close">Close</div></button>
+            {workout.workout_name} - Sets: {workout.sets}, Reps: {workout.reps}, Duration: {workout.duration} minutes
+            <button className="delete-button" onClick={() => handleDeleteWorkout(workout.id)}>
+              <span className="X"></span>
+              <span className="Y"></span>
+              <div className="close">Close</div>
+            </button>
           </li>
         ))}
       </ul>
-      <button className='delete-all' onClick={handleDeleteAllWorkouts}> Delete all</button>
+      <button className="delete-all" onClick={handleDeleteAllWorkouts}>
+        Delete all
+      </button>
+
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h4>Add Exercise</h4>
+            <label>Exercise name:</label>
+            <input type="text" value={selectedExercise} onChange={handleExerciseChange} />
+            <label>Sets:</label>
+            <input type="number" value={sets} onChange={handleSetsChange} />
+            <label>Reps:</label>
+            <input type="number" value={reps} onChange={handleRepsChange} />
+            <label>Duration (in minutes):</label>
+            <input type="number" value={duration} onChange={handleDurationChange} />
+            <button onClick={handleModalClose}>Cancel</button>
+            <button onClick={handleModalSubmit}>Save</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 export default WorkoutPlanner;
